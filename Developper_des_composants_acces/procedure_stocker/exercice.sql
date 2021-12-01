cb27aa0724163396119240e5654e82e6304dc374
harold <s.lefebvre907@laposte.net>
2021-12-01 09:39:55 +0100

    mise à jour exo

--- Developper_des_composants_acces/procedure_stocker/exercice.sql
+++ Developper_des_composants_acces/procedure_stocker/exercice.sql
@@ -6,7 +6,11 @@
 DELIMITER  $$
 CREATE PROCEDURE List_fournis()
 begin
-    SELECT fournis.numfou AS Numero_fournisseur, produit.libart AS produit_vendu  FROM ligcom JOIN entcom ON entcom.numcom = ligcom.numcom JOIN fournis ON fournis.numfou = entcom.numfou JOIN produit ON produit.codart = ligcom.codart;
+    SELECT fournis.numfou AS Numero_fournisseur, produit.libart AS produit_vendu
+    FROM ligcom
+             JOIN entcom ON entcom.numcom = ligcom.numcom
+             JOIN fournis ON fournis.numfou = entcom.numfou
+             JOIN produit ON produit.codart = ligcom.codart;
 end $$
 DELIMITER ;
 
@@ -15,7 +19,39 @@ DELIMITER ;
 DELIMITER  $$
 CREATE PROCEDURE List_fournis()
 begin
-    SELECT fournis.numfou AS Numero_fournisseur, produit.libart AS produit_vendu  FROM ligcom JOIN entcom ON entcom.numcom = ligcom.numcom JOIN fournis ON fournis.numfou = entcom.numfou JOIN produit ON produit.codart = ligcom.codart;
+    SELECT entcom.numcom        AS 'numéro de commande',
+           fournis.nomfou       AS 'nom du fournisseur',
+           produit.libart       AS 'libellé du produit',
+           SUM(qtecde * priuni) as 'Sous total'
+    FROM ligcom
+             JOIN produit ON produit.codart = ligcom.codart
+             JOIN entcom ON entcom.numcom = ligcom.numcom
+             JOIN fournis ON fournis.numfou = entcom.numfou
+    WHERE obscom LIKE '%urgent%'
+    GROUP BY entcom.numcom, nomfou, libart;
 end $$
 DELIMITER ;
 
+
+#Exercice 3 : création d'une procédure stockée avec plusieurs paramètres
+#Créer la procédure stockée CA_ Fournisseur, qui pour un code fournisseur et une année entrée en paramètre, calcule et restitue le CA potentiel de ce fournisseur pour l'année souhaitée (cette requête sera construite à partir de la requête n°19).
+
+#On exécutera la requête que si le code fournisseur est valide, c'est-à-dire dans la table FOURNIS.
+
+DELIMITER  $$
+CREATE PROCEDURE CA_fournisseur(In Year int(4))
+begin
+    SELECT SUM(
+                   ligcom.priuni * ligcom.qtecde * 1.2) as `chiffre d'affaire`
+
+    from entcom
+             JOIN ligcom ON ligcom.numcom = entcom.numcom
+             JOIN fournis on entcom.numfou = fournis.numfou
+    WHERE YEAR(ligcom.derliv) = Year
+    GROUP BY entcom.numfou;
+end $$
+DELIMITER ;
+
+#Testez cette procédure avec différentes valeurs des paramètres.
+call CA_fournisseur(2007);
+call CA_fournisseur(2000);
