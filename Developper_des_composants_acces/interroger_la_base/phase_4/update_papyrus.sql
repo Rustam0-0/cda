463baeb7acc23b20676b8fa6b9233b3a64353681
harold <s.lefebvre907@laposte.net>
2021-12-01 09:56:57 +0100

    mise à jour exo

--- Developper_des_composants_acces/interroger_la_base/phase_4/update_papyrus.sql
+++ Developper_des_composants_acces/interroger_la_base/phase_4/update_papyrus.sql
@@ -1,13 +1,12 @@
---1 Application d'une augmentation de tarif de 4% pour le prix1, 2% pour le prix2 pour le fournisseur 9180
+#1 Application d'une augmentation de tarif de 4% pour le prix1, 2% pour le prix2 pour le fournisseur 9180
 UPDATE `vente` SET `prix1`= prix1 + 0.4 and prix2=prix2 + 0.12 where numfou =9180
 
---2. Dans la table vente, mettre à jour le prix2 des articles dont le prix2 est null, en affectant a valeur de prix.
+#2. Dans la table vente, mettre à jour le prix2 des articles dont le prix2 est null, en affectant a valeur de prix.
 update vente set prix2 = prix1 where prix2 is null
---3. Mettre à jour le champ obscom en positionnant '*****' pour toutes les commandes dont le fournisseur a
--- un indice de satisfaction <5
-UPDATE fournis f INNER JOIN entcom e ON f.numfou = e.numfou SET e.obscom = '*****' WHERE f.satisf < 5
---4. Suppression du produit I110
+#3. Mettre à jour le champ obscom en positionnant '*****' pour toutes les commandes dont le fournisseur a un indice de satisfaction <5
+UPDATE fournis f INNER JOIN entcom e ON f.numfou = e.numfou SET e.obscom = '*****' WHERE f.satisf < 5;
+#4. Suppression du produit I110
 DELETE FROM vente WHERE codart = 'I110';
 DELETE FROM produit  WHERE codart = 'I110';
---5. Suppression des entête de commande qui n'ont aucune ligne
+#5. Suppression des entête de commande qui n'ont aucune ligne
 DELETE e FROM entcom e LEFT JOIN ligcom l on e.numcom= l.numcom WHERE l.numcom IS NULL
