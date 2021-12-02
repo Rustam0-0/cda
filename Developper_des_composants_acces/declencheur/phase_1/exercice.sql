86a7abdc694eba3eed6f36400cfd4013ece8a0e8
harold <s.lefebvre907@laposte.net>
2021-12-02 16:15:45 +0100

    mise à jour exo

--- Developper_des_composants_acces/declencheur/phase_1/exercice.sql
+++ Developper_des_composants_acces/declencheur/phase_1/exercice.sql
@@ -71,3 +71,68 @@ BEGIN
     END IF;
 END $$
 DELIMITER ;
+
+
+
+#travail à réaliser
+DELIMITER $$
+CREATE TRIGGER maj_total AFTER INSERT ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande;
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c);
+    UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+DELIMITER ;
+
+#1 Mettez en place ce trigger, puis ajoutez un produit dans une commande, vérifiez que le champ total est bien mis à jour.
+INSERT INTO `lignedecommande` (`id_commande`, `id_produit`, `quantite`, `prix`) VALUES ('2', '3', '3', '10');
+
+
+#2 Ce trigger ne fonctionne que lorsque l'on ajoute des produits dans la commande, les modifications ou suppressions ne permettent pas de
+# recalculer le total. Modifiez le code ci-dessus pour faire en sorte
+# que la modification ou la suppression de produit recalcul le total de la commande.
+##for update
+DELIMITER $$
+CREATE TRIGGER maj_total_update AFTER update ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande;
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c);
+    UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+DELIMITER ;
+
+
+##delete
+DELIMITER $$
+CREATE TRIGGER maj_total_delete AFTER delete ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = old.id_commande;
+    SET tot = (SELECT sum(prix*quantite) FROM lignedecommande WHERE id_commande=id_c);
+    UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+DELIMITER ;
+
+#3 Un champ remise était prévu dans la table commande. Prenez en compte ce champ dans le code de votre trigger.
+
+DELIMITER $$
+CREATE TRIGGER maj_total_remise
+    AFTER INSERT
+    ON lignedecommande
+    FOR EACH ROW
+BEGIN
+    DECLARE id_c INT;
+    DECLARE tot DOUBLE;
+    SET id_c = NEW.id_commande;
+    SET tot = (SELECT sum(prix*quantite)-remise FROM lignedecommande JOIN commande ON commande.id=lignedecommande.id_commande WHERE id_commande=id_c);
+    UPDATE commande SET total=tot WHERE id=id_c;
+END $$
+DELIMITER ;
