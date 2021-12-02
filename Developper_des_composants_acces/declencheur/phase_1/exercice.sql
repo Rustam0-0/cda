19921dd8fdf34593012b72b1e0b07b4571a6e0dc
harold <s.lefebvre907@laposte.net>
2021-12-02 13:54:18 +0100

    mise à jour exo

--- Developper_des_composants_acces/declencheur/phase_1/exercice.sql
+++ Developper_des_composants_acces/declencheur/phase_1/exercice.sql
@@ -31,8 +31,10 @@ CREATE TRIGGER insert_reservation
 BEGIN
     DECLARE nombre_reservation INT;
     DECLARE chambreid INT;
+    DECLARE numhotel INT;
     SET chambreid = NEW.res_cha_id;
-    SET nombre_reservation = (SELECT COUNT(res_cha_id) FROM reservation JOIN chambre ON cha_id=res_cha_id  where res_cha_id=chambreid GROUP BY cha_hot_id);
+    SET numhotel = (SELECT cha_hot_id FROM reservation JOIN chambre ON cha_id=res_cha_id  where res_cha_id=chambreid GROUP BY cha_hot_id);
+    SET nombre_reservation = (SELECT COUNT(res_cha_id) FROM reservation JOIN chambre ON cha_id=res_cha_id  where  cha_hot_id=numhotel);
     IF nombre_reservation>10 THEN
         SIGNAL SQLSTATE '40000' SET MESSAGE_TEXT = 'Rèservation non autorisée !';
     END IF;
@@ -51,6 +53,21 @@ BEGIN
     SET nombre_cli_reservation = (SELECT COUNT(res_cli_id) FROM reservation where res_cli_id=clientid);
     IF nombre_cli_reservation>3 THEN
         SIGNAL SQLSTATE '40000' SET MESSAGE_TEXT = 'le client à trop réservé !';
+    END IF;
+END $$
+DELIMITER ;
+
+#4insert_chambre : lors d'une insertion, on calcule le total des capacités des chambres pour l'hôtel, si ce total est supérieur à 50, on interdit l'insertion de la chambre.
+DELIMITER $$
+CREATE TRIGGER insert_chambre
+    AFTER INSERT
+    ON chambre
+    FOR EACH ROW
+BEGIN
+    DECLARE nombre_chambre_max INT;
+    SET nombre_chambre_max = (SELECT sum(`cha_capacite`) from chambre where cha_hot_id=NEW.cha_hot_id);
+    IF nombre_chambre_max>50 THEN
+        SIGNAL SQLSTATE '40000' SET MESSAGE_TEXT = 'Aucun chambre disponible!';
     END IF;
 END $$
 DELIMITER ;
