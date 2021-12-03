946513782986c7109dcb2f6cdb0f56ababf9f17a
harold <s.lefebvre907@laposte.net>
2021-12-03 09:25:16 +0100

    mise à jour exo

--- Developper_des_composants_acces/declencheur/phase_2/exercice.sql
+++ Developper_des_composants_acces/declencheur/phase_2/exercice.sql
@@ -1,3 +1,13 @@
+#Create table ARTICLES_A_COMMANDER
+DROP DATABASE IF EXISTS `ARTICLES_A_COMMANDER`;
+CREATE TABLE `ARTICLES_A_COMMANDER`
+(
+    `CODART` char(4) NOT NULL,
+    `DATE`   date    NOT NULL DEFAULT current_timestamp(),
+    `QTE`    int(11) NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
 #Mysql : les déclencheurs, Papyrus
 
 DELIMITER $$
@@ -8,7 +18,7 @@ CREATE TRIGGER after_products_update
     FOR EACH ROW
 BEGIN
     IF NEW.stkale < NEW.stkphy THEN
-        INSERT INTO ARTICLES_A_COMMANDER (QTE,CODART) VALUES ((NEW.stkale - NEW.stkphy),NEW.CODART);
+        INSERT INTO ARTICLES_A_COMMANDER (QTE, CODART) VALUES ((NEW.stkale - NEW.stkphy), NEW.CODART);
     END IF;
 END;
 $$
