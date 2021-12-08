251569f76327a5325e6743be572946f1dad9a96d
harold <s.lefebvre907@laposte.net>
2021-12-08 14:26:52 +0100

    mise à jour exo

--- fil_rouge/alimenter_la_base/exercice.sql
+++ fil_rouge/alimenter_la_base/exercice.sql
@@ -48,17 +48,62 @@ VALUES (NULL, 'françois-régis', 'caumartin', '0663534369', '8', '6', '1');
 
 ## entré des catégories
 
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Guit/Bass', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Batteries', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Clavier', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Studio', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Sono', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Eclairage', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('DJ', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Cases', NULL);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Accessoires', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Guit/Bass', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Batteries', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Clavier', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Studio', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Sono', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Eclairage', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('DJ', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Cases', NULL);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Accessoires', NULL);
 
 ##insertion de sous catégorie de Guit/bass
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Guitares Electriques', 1);
-INSERT INTO `categories` ( `cat_name`, `cat_id_parent`) VALUES ('Guitares Classiques', 1);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Guitares Electriques', 1);
+INSERT INTO `categories` (`cat_name`, `cat_id_parent`)
+VALUES ('Guitares Classiques', 1);
 
+
+#entré de 3 clients avec le commercial françois-regis :)
+INSERT INTO `customers` (`cus_firstname`, `cus_lastname`, `cus_telephone`, `cus_type`, `cus_coef`, `emp_id`)
+VALUES ('Harold', 'Lefebvre', '0610012548', 'particulier', '60', '1'),
+       ('Emmanuel', 'Berthome', '', 'particulier', '60', '1'),
+       ('Marion', 'Joly', '060837188', 'particulier', '60', '1');
+
+
+#entré d'association avec l'entité live pour l'adresse du client
+INSERT INTO `habiter` (`cus_id`, `Id_Live`)
+VALUES ('1', '7');
+INSERT INTO `habiter` (`cus_id`, `Id_Live`)
+VALUES ('2', '8');
+INSERT INTO `habiter` (`cus_id`, `Id_Live`)
+VALUES ('3', '9');
+
+# entré de produit
+INSERT INTO `products` (`pro_name`, `pro_description`, `pro_photo`, `pro_label`, `pro_ref`, `pro_price`,
+                        `pro_status`, `pro_stock`, `cat_id`, `sup_id`)
+VALUES ('Fender Squier Affinity Strat MN BK',
+        'Série Affinity Corps en peuplier Manche en érable Profil du manche en C Touche en érable Diapason: 648 mm (25,5\")',
+        'jpg', 'guitare', 'gui123', '200.00', '1', '20', '10', '3'),
+       ('Startone CG 851 1/8', 'Taille: 1/8 Corps en tilleul Manche en nato Touche en érable', 'jpg',
+        'guitare startone', 'gui124', '220.00', '1', '20', '11', '2');
+
+#entré de commande dans order
+INSERT INTO `orders` (`ode_order_date`, `ord_date_payment`, `ord_date_ship`, `ord_date_delivery`,
+                      `ord_status`, `ord_billing_address`, `ord_shippen_address`, `cus_id`)
+VALUES ('2021-12-08 14:23:22', '2021-12-08 14:23:22', '2021-12-08 14:23:22', '2021-12-16 14:23:22', 'expédié',
+        '7', '8', '1');
+
+#entré de ligne de commande
+INSERT INTO `orders_details` (`ode_unit_price`, `ode_qte`, `ode_discount`, `pro_id`, `ord_id`)
+VALUES ('200', '2', '70', '1', '1');
