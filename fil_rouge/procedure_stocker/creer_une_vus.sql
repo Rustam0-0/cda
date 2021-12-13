6b5ce501655c3d97199bd42d28c6489dcba00514
harold <s.lefebvre907@laposte.net>
2021-12-13 16:40:37 +0100

    mise à jour exo

--- fil_rouge/procedure_stocker/creer_une_vus.sql
+++ fil_rouge/procedure_stocker/creer_une_vus.sql
@@ -1,4 +1,14 @@
 #Créez une vue correspondant à la jointure Produits - Fournisseurs
-CREATE VIEW  product_fournis
+CREATE VIEW product_fournis
 AS
-SELECT pro_id, pro_name, suppliers.sup_id FROM `products` join suppliers ON products.sup_id=suppliers.sup_id;
+SELECT pro_id, pro_name, suppliers.sup_id
+FROM `products`
+         join suppliers ON products.sup_id = suppliers.sup_id;
+
+
+CREATE VIEW product_fournis_categorie
+AS
+SELECT pro_id, pro_name, suppliers.sup_id, categories.cat_id, categories.cat_id_parent
+FROM `products`
+         join suppliers ON products.sup_id = suppliers.sup_id
+         right JOIN categories  on products.cat_id=categories.cat_id ;
